2b59ce9168aed0e1e73dd3900b60bb83933423c2
Krzysztof Begiedza <contact@kbegiedza.eu>
2023-01-09 19:53:23 +0000

    BytesEntity

---
+++ src/CrushDB/Entities/BytesEntity.cs
@@ -0,0 +1,18 @@
+namespace CrushDB.Entities
+{
+    public class BytesEntity
+    {
+        public BytesEntity(string partitionKey, string rowKey, int size)
+        {
+            RowKey = rowKey;
+            PartitionKey = partitionKey;
+            Bytes = new byte[size * 8];
+        }
+
+        public byte[] Bytes { get; set; }
+
+        public string RowKey { get; set; }
+
+        public string PartitionKey { get; set; }
+    }
+}
--- src/CrushDB/Program.cs
+++ src/CrushDB/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CrushDB.Entities;
+
+Console.WriteLine("Hello, World!");
+
+var sampleEntity = new BytesEntity($"{Guid.NewGuid()}+{Guid.NewGuid()}", $"{Guid.NewGuid()}", 1024);
